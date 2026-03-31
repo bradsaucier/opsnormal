e758732bebd3b19d2d124609dbace8c856b52985
Bradley Saucier <bradley.saucier@snhu.edu>
2026-03-31 17:06:39 -0400

    Allow service workers in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   use: {
     baseURL: 'http://127.0.0.1:4173',
     trace: 'on-first-retry',
+    serviceWorkers: 'allow',
   },
   webServer: {
     command: 'npm run preview -- --host 127.0.0.1 --port 4173',
