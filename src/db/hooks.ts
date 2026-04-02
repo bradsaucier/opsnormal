52b6d7aba871aa08d3902d5d78e00289f7775dec
Bradley Saucier <bradley.saucier@snhu.edu>
2026-04-02 08:34:41 -0400

    Refactor useEntriesForDate and useEntriesForDateRange

--- src/db/hooks.ts
+++ src/db/hooks.ts
@@ -1,14 +1,27 @@
+import Dexie from 'dexie';
 import { useLiveQuery } from 'dexie-react-hooks';
 
 import { db } from './appDb';
 
 export function useEntriesForDate(date: string) {
-  return useLiveQuery(() => db.dailyEntries.where('date').equals(date).toArray(), [date], []);
+  return useLiveQuery(
+    () =>
+      db.dailyEntries
+        .where('[date+sectorId]')
+        .between([date, Dexie.minKey], [date, Dexie.maxKey], true, true)
+        .toArray(),
+    [date],
+    []
+  );
 }
 
 export function useEntriesForDateRange(startDate: string, endDate: string) {
   return useLiveQuery(
-    () => db.dailyEntries.where('date').between(startDate, endDate, true, true).toArray(),
+    () =>
+      db.dailyEntries
+        .where('[date+sectorId]')
+        .between([startDate, Dexie.minKey], [endDate, Dexie.maxKey], true, true)
+        .toArray(),
     [startDate, endDate],
     []
   );
