554dbf4c57fdc01b2f15f999e93063554ada46f4
Bradley Saucier <bradley.saucier@snhu.edu>
2026-04-04 09:22:02 -0400

    Change import path for useRegisterSW in usePwaUpdate
    Signed-off-by: Bradley Saucier <bradley.saucier@snhu.edu>

--- src/features/pwa/usePwaUpdate.ts
+++ src/features/pwa/usePwaUpdate.ts
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { useRegisterSW } from 'virtual:pwa-register/react';
+import { useRegisterSW } from './registerSw';
 
 import { reloadCurrentPage } from '../../lib/runtime';
 
