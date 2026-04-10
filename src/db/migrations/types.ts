d0570e7d45139ae4b4d0c5784fb9a5fff494853f
Bradley Saucier <bradley.saucier@snhu.edu>
2026-04-10 19:11:21 -0400

    Add types for database migrations
    Signed-off-by: Bradley Saucier <bradley.saucier@snhu.edu>

---
+++ src/db/migrations/types.ts
@@ -0,0 +1,13 @@
+import type { Transaction } from 'dexie';
+
+export type OpsNormalDbStores = Record<string, string | null>;
+export type OpsNormalDbMigrationUpgrade = (
+  transaction: Transaction
+) => PromiseLike<void> | void;
+
+export interface OpsNormalDbMigration {
+  version: number;
+  name: string;
+  stores: OpsNormalDbStores;
+  upgrade?: OpsNormalDbMigrationUpgrade;
+}
